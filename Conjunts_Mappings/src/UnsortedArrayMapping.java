0093699c845e81de923e97148c26395657c0225c
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-05-29 22:02:44 +0200

    Update UnsortedArrayMapping.java

--- Conjunts_Mappings/src/UnsortedArrayMapping.java
+++ Conjunts_Mappings/src/UnsortedArrayMapping.java
@@ -1,7 +1,7 @@
 public class UnsortedArrayMapping <K,V>{
 
-    private K[] claus;
-    private V[] valors;
+    private K[] keys;
+    private V[] values;
     private int n;
 
     /**
@@ -10,14 +10,69 @@ public class UnsortedArrayMapping <K,V>{
      * @param max
      */
     public UnsortedArrayMapping(int max){
-        claus= (K[]) new Object[max];
-        valors=(V[]) new Object()[max];
+        keys= (K[]) new Object[max];
+        values=(V[]) new Object[max];
          n=0;
     }
 
-    public V get(K key){}
+    public V get(K key){
 
-    public V put (K key, V value){}
+        for (int i = 0; i < n; i++) {
+            if (keys[i].equals(key)){
+                return values[i];
+            }
 
-    publi cV remove ()
+        }
+        return null;
+    }
+
+    /**
+     * Adds the pair (key,value) to se mapping. It the key already exists we update it and returns the previw value
+     * Esle we return null
+     * @param key
+     * @param value
+     * @return
+     */
+    public V put (K key, V value){
+        //if the key already exists, we update its value
+        for (int i = 0; i < n; i++) {
+            if (keys[i].equals(key)){
+                V previous = values[i];//get preview value for the key
+                values[i]=value;
+                return previous;
+
+            }
+
+        }
+        //Can we add it?
+        if (n< keys.length){
+            keys[n]=key;
+            values[n++]=value;
+        }
+        return null;
+
+
+
+    }
+
+    /**
+     * Rremoves the key from the mapping and its values.
+     * @param key
+     * @return the value of the key
+     */
+    public V remove(K key){
+        for (int i = 0; i < n; i++) {
+            if (keys[i].equals(key)){
+                V value = values[i];
+                keys[i]=keys[n-1];
+                values[i]=values[n-1];
+                n--;
+                return value;
+            }
+
+        }
+        return null;
+
+    }
+
 }
