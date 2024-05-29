2aa4cab9f124405ab383eabbaf6d8d39a84b508a
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-05-29 22:04:08 +0200

    Update UnsortedLinkedListMapping.java

--- Conjunts_Mappings/src/UnsortedLinkedListMapping.java
+++ Conjunts_Mappings/src/UnsortedLinkedListMapping.java
@@ -1,88 +1,104 @@
-public class UnsortedLinkedListMapping<K,V> {
-    private class Node {
-        private K key;
-        private V value;
-        private Node next;
+import java.util.Iterator;
+import java.util.Vector;
 
-        public Node(K key, V value, Node next){
-            this.value=value;
-            this.key=key;
-            this.next=next;
+public class UnsortedLinkedListMapping<K, V> {
+    private class Node {
+        private final K key;
+        private final Node next;
+        private V value;
+
+        public Node(K key, V value, Node next) {
+            this.value = value;
+            this.key = key;
+            this.next = next;
         }
+    }
+
+    public Iterator iterator(){
+        return new UnsortedLinkedListMappingIterator();
+    }
+    private class UnsortedLinkedListMappingIterator implements Iterator{
+
+    private Node iterator;
+        @Override
+        public boolean hasNext() {
+            return iterator!=null;
+        }
+
+        @Override
+        public Object next() {
+           Pair next = new Pair(iterator.key, iterator.value);
+           iterator= iterator.next;
+           return next;
+        }
+    }
+    private class Pair{
+        private final K key;
+        private final V value;
+        public Pair(K key, V value){
+            this.key=key;
+            this.value=value;
+        }
+
     }
     private Node first;
 
     public V get(K key) {
         Node current = first;
         boolean trobat = false;
-        while (current!=null && !trobat){
-
-            trobat = current.key.equals(key);
-            if (!trobat){
-                current=current.next;
+        while (current != null) {
+            if (current.key.equals(key)) {
+                return current.value;
             }
-        }
-        if (trobat){
-            return current.value;
-        }else{
-            return null;
-        }
+            current = current.next;
 
-
+        }
+        return null;
     }
+
     public V put(K key, V value) {
 
         Node current = first;
-        boolean trobat = false;
-        while (current!=null && !trobat){
-            trobat = current.key.equals(key);
-            if (!trobat){
-                current=current.next;
+        while (current != null) {
+            if (current.key.equals(key)) {
+                V previous = current.value;
+                current.value = value;//update the value
+                return previous;
             }
-
-        }
-        if (trobat){
-            V valuenprevi = current.value;
-            current.value=value;
-            return valuenprevi;
-        }else{
-            Node n = new Node(key,value,first);
-            first = n;
-            return null;
+            current = current.next;
         }
 
+
+        //creates a new node and add it to the start of the mapping
+        Node tmp = new Node(key, value, first);
+        first = tmp;
+        return null;
     }
+
     public V remove(K key) {
         Node current = first;
         Node previous = null;
         boolean trobat = false;
-        while (current != null && !trobat) {
-            trobat = current.key.equals(key);
-            if (!trobat) {
-                previous = current;
-                current = current.next;
-            }
-            }
-
-            if (trobat) {
-                V valueprevi = current.value;
+        while (current != null) {
+            if (current.key.equals(key)) {
+                V tmp = current.value;
                 if (previous == null) {
                     first = current.next;
-
                 } else {
-                    previous.next = current.next;}
-                    return valueprevi;
-
-            } else {
-                return null;
+                    previous = current.next;
+                }
             }
+            previous = current;
+            current = current.next;
         }
-
+        return null;
+    }
 
     public boolean isEmpty() {
-            return first==null;
-        }
-
+        return first == null;
     }
+
+
+}
 
 
